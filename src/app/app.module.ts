c8236be11672406ee316927b8cff9e55c1fb368d
Mike Tinnes <mtinnes@gmail.com>
2020-09-25 12:13:28 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { APP_BASE_HREF, registerLocaleData } from '@angular/common';
+
+import { PlayerClientModule } from '@reveldigital/player-client';
 
 import { AppComponent } from './app.component';
+
+import localeFr from '@angular/common/locales/fr';
+import localeRu from '@angular/common/locales/ru';
+registerLocaleData(localeFr);
+registerLocaleData(localeRu);
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PlayerClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useFactory: () => {
+        try {
+          return new gadgets.Prefs().getLang();
+        } catch {
+          return 'en';
+        }
+      }
+    },
+    { provide: APP_BASE_HREF, useValue: '/gadgets/ifr' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
